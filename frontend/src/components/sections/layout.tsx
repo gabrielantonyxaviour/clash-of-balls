7d42b52c444382d3897da870a42250b745f7a2c9
Jasmine K <jasmine30052002@gmail.com>
2024-09-05 16:51:17 +0530

    build fix

--- frontend/src/components/sections/layout.tsx
+++ frontend/src/components/sections/layout.tsx
@@ -3,19 +3,27 @@
 interface LayoutProps {
   children: React.ReactNode;
 }
-import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
 import Navbar from "./navbar";
 import { useState } from "react";
 import Steps from "./create/Steps";
+
 export default function Layout({ children }: LayoutProps) {
+  const [steps, setSteps] = useState(0);
+  return (
+    <Suspense>
+      <div className="max-w-[800px] mx-auto h-screen bg-card">
+        <Steps step={1} />
+        <NavbarComponent />
+      </div>
+    </Suspense>
+  );
+}
+
+function NavbarComponent() {
   const searchParams = useSearchParams();
 
   const fid = searchParams.get("fid");
-  const [steps, setSteps] = useState(0);
-  return (
-    <div className="max-w-[800px] mx-auto h-screen bg-card">
-      <Steps step={1} />
-      <Navbar fid={fid != undefined ? fid : "-1"} />
-    </div>
-  );
+  return <Navbar fid={fid != undefined ? fid : "-1"} />;
 }
