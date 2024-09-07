6fd925a77a01276839c4606c8f47f844c1b28345
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-09-07 11:13:09 +0530

    add farcaster button

--- frontend/src/components/sections/challenge/page.tsx
+++ frontend/src/components/sections/challenge/page.tsx
@@ -5,29 +5,40 @@ import MatchCard from "../common/match-card";
 import FanTokenBalances from "../common/fan-token-balances";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import FarcasterButton from "@/components/ui/custom/farcaster-button";
+import { useAccount } from "wagmi";
 
 export default function Challenge({ id }: { id: string }) {
+  const { address } = useAccount();
   useEffect(() => {}, [id]);
   return (
     <div className="">
       <p className="pt-2 text-center text-lg font-semibold">Challenge</p>
 
       <MatchCard gameId={0} />
-      <p className="text-md font-semibold text-center px-4">Proposed Amount</p>
-      <div className="flex justify-center items-center mt-2 space-x-3 p-[6px] w-full">
-        <p className="font-bold text-xl">10</p>
-        <Image
-          src={"/coins/chiliz.png"}
-          height={30}
-          width={30}
-          alt="chiliz"
-          className="rounded-full"
-        />
+      <div className="flex justify-around my-5">
+        <div>
+          <p className="text-md font-semibold text-center mb-2">Proposed By</p>
+          <FarcasterButton fid={"249577"} address="0x" />
+        </div>
+        <div>
+          <p className="text-md font-semibold text-center">Proposed Amount</p>
+          <div className="flex justify-center items-center mt-2 space-x-3 p-[6px] w-full">
+            <p className="font-medium text-lg">10</p>
+            <Image
+              src={"/coins/chiliz.png"}
+              height={25}
+              width={25}
+              alt="chiliz"
+              className="rounded-full"
+            />
+          </div>
+        </div>
       </div>
 
       <FanTokenBalances gameId={0} />
 
-      <div className="flex justify-center space-x-3 mt-12">
+      <div className="flex justify-center space-x-3 mt-8">
         <Button variant={"ghost"}>Negotiate</Button>
         <Button>Accept</Button>
       </div>
---
+++ frontend/src/components/ui/custom/farcaster-button.tsx
@@ -0,0 +1,88 @@
+import { useQuery } from "@airstack/airstack-react";
+import { Data, QueryResponse } from "@/lib/airstackInterface";
+import { useAccount } from "wagmi";
+import { useEffect, useState } from "react";
+import { Button } from "../button";
+import Image from "next/image";
+
+export default function FarcasterButton({
+  fid,
+  address,
+}: {
+  fid: string;
+  address: string;
+}) {
+  const [profileImage, setProfileImage] = useState("");
+  const [fName, setFName] = useState("");
+  const [localFid, setLocalFid] = useState("-1");
+  const [fetched, setFetched] = useState(false);
+  const query =
+    fid != "-1"
+      ? `  query MyQuery {
+  Socials(
+    input: {filter: {dappName: {_eq: farcaster}, userId: {_eq: "${fid}"}}, blockchain: ethereum}
+  ) {
+    Social {
+      profileImage
+      profileHandle
+      userId
+    }
+  }
+}`
+      : ` query MyQuery {
+  Socials(
+    input: {blockchain: ethereum, filter: {dappName: {_eq: farcaster}, identity: {_eq: "${address}"}}}
+  ) {
+    Social {
+      profileImage
+      profileHandle
+      userId
+    }
+  }
+}`;
+  const {
+    data,
+    loading,
+    error: queryError,
+  }: QueryResponse = useQuery<Data>(query, {}, { cache: false });
+  useEffect(() => {
+    console.log("HI");
+    console.log(fid);
+    if (
+      data != null &&
+      (data as any).Socials != null &&
+      (data as any).Socials.Social != null &&
+      (data as any).Socials.Social.length > 0
+    ) {
+      setProfileImage((data as any).Socials.Social[0].profileImage);
+      setFName((data as any).Socials.Social[0].profileHandle);
+      console.log(data);
+      setLocalFid(fid);
+      setFetched(true);
+    } else {
+      console.log("No profile found");
+      console.log(data);
+    }
+  }, [data, loading, queryError]);
+  return (
+    localFid != "-1" && (
+      <Button
+        variant="ghost"
+        onClick={() => {
+          window.open(`https://warpcast.com/${fName}`, "_blank");
+        }}
+      >
+        <div className="w-[20px] h-[20px] overflow-hidden relative rounded-full">
+          <Image
+            src={profileImage}
+            alt="avatar"
+            width={20}
+            height={20}
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <p className="ml-2">{fName}</p>
+      </Button>
+    )
+  );
+}
